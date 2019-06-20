1aa0645d516222b911c2034721100ad6a90b2c40
zinuo li <zinuo.lee@gmail.com>
2019-06-20 09:29:04 -0500

    merge

--- src/com/laicode/base/binary_search/SearchInSortedMatrixI.java
+++ src/com/laicode/base/binary_search/SearchInSortedMatrixI.java
@@ -22,13 +22,52 @@ public class SearchInSortedMatrixI {
 
     static int[] search(int[][] matrix, int target) {
         // Consider 2D matrix as an array
-        int cols = matrix.length;
-        int rows = matrix[0].length;
-        int[] result = new int[]{-1,-1};
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return new int[]{ -1, -1 };
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
         int left = 0, right = cols * rows -1;
         while (left <= right) {
-
+            int mid = left + (right - left) / 2;
+            int row = mid / cols;
+            int col = mid % cols;
+            if (matrix[row][col] == target) {
+                return new int[]{row, col};
+            } else if (matrix[row][col] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
         return new int[]{-1,-1};
+    }
+
+    static int[] searchII(int[][] matrix, int target) {
+        // search row first, then search col
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return new int[]{ -1, -1 };
+        }
+        int rows = matrix.length; // 0~rows-1 is index
+        int up = 0, down = rows - 1;
+        while (up < down) { // target should fall in up row
+            int mid = up + (down - up) / 2;
+            if (matrix[mid][0] <= target && matrix[mid + 1][0] > target) {
+                int col = ClassicBinarySearch.binarySearch(matrix[mid], target);
+                return new int[]{mid, col};
+            } else if (matrix[mid][0] > target) {
+                down = mid;
+            } else {
+                up = mid;
+            }
+        }
+        return new int[]{ -1, -1 };
+    }
+
+
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1,2,3},{4,5,7},{8,9,10},{12,14,16}};
+        System.out.println(matrix.length);
     }
 }
